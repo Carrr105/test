711b033b8c94db4ee6e6a755712d43469424344d
Carrr105 <carlosger_64@hotmail.com>
2020-05-13 11:08:54 -0500

    aaaaaaaaaaXD

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -12,10 +12,12 @@ public class DesktopLauncher {
 }
 
 
-// commit guardar cambios
-// push mandar nuevo codigo
+// commit guardar cambios (localmente)
+// push mandar nuevo codigo (se manda con todos los cambios)
 // pull sacar un proyecto existente
+// import from vcs o clone sacar un proyecto existente
 // merge combinar branches
 //XD
 // L O L
 // le zas
+
